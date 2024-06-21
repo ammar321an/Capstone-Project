62e62b96cf3530d7cbd5474ecc5e427aa731b3ce
Muhammad Ammar Yaseer <ammar321an@users.noreply.github.com>
2024-06-22 02:06:30 +0800

    Revert "Updated Code"
    This reverts commit f855f9159d4a90443e4b7946a379852df9da0691.

--- esp32/Finalize/Secret.h
+++
@@ -1,5 +0,0 @@
-char ssid[] = ""; //Enter your WIFI SSID
-char pass[] = "";   //Enter your WIFI password
-
-String phoneNumber = "";    // Enter your phone number                             
-String apiKey = "";        // Enter whatsapp API key
